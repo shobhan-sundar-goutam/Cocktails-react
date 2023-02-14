5a7fbe8bc62e937ebe10a647df7918a1c5113523
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-02-14 20:31:54 +0530

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
