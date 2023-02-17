329fc1e717bfbaeb51c7a7fb772a4bfbb2845ef6
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-02-17 14:03:41 +0530

    cocktail

---
+++ src/components/Cocktail.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+import { useGlobalContext } from "../context";
+
+const Cocktail = () => {
+  const { cocktails, getDrinkDetails } = useGlobalContext();
+
+  return (
+    <>
+      {cocktails.map((drink) => {
+        const { idDrink, strDrink, strGlass, strCategory, strDrinkThumb } =
+          drink;
+        return (
+          <article className="cocktail" key={idDrink}>
+            <div className="img-container">
+              <img src={strDrinkThumb} alt={strDrink} />
+            </div>
+            <div className="cocktail-footer">
+              <h3>{strDrink}</h3>
+              <h4>{strGlass}</h4>
+              <p>{strCategory}</p>
+              <NavLink
+                className="btn btn-primary btn-details"
+                to={"/singlecocktail"}
+                onClick={() => getDrinkDetails(idDrink)}
+              >
+                details
+              </NavLink>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default Cocktail;
