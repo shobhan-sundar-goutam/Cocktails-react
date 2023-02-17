b8020b77abe72beae9c167afb3046ab51aa97b33
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-02-17 17:50:34 +0530

    no matched cocktail

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -2,6 +2,16 @@ import { useGlobalContext } from "../context";
 import Cocktail from "./Cocktail";
 
 const CocktailList = () => {
+  const { cocktails } = useGlobalContext();
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        no cocktails matched your search criteria
+      </h2>
+    );
+  }
+
   return (
     <section className="section">
       <h2 className="section-title">cocktails</h2>
